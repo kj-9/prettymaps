8e31bc2dcf85d0c317e163636220b46d3bd7ca53
kj-9 <46800811+kj-9@users.noreply.github.com>
2023-07-10 08:10:03 +0900

    add tests

---
+++ tests/test_plot.py
@@ -0,0 +1,24 @@
+
+from prettymaps import plot, presets
+
+
+def test_presets():
+    assert len(presets()) == 7
+
+
+def test_plot(image_regression, datadir):
+    """
+    needs pytest-regression package.
+    run by `pythom -m test tests/`
+    force regen image by `pythom -m test tests/ --force-regen`
+    see also: https://pytest-regressions.readthedocs.io/en/latest/index.html
+    """
+    image = datadir / "macau-circle-300.png"
+    print(str(image))
+    plot(
+        "Praça Ferreira do Amaral, Macau",
+        circle=True,
+        radius=300,
+        save_as=str(image),
+    )
+    image_regression.check(image.read_bytes(), diff_threshold=5)
