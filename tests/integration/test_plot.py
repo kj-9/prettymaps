730185d646bcc7bdf27ac287dc44e0a266d0093c
kj-9 <46800811+kj-9@users.noreply.github.com>
2023-07-14 09:26:40 +0900

    add new api file and tests, refs #7

---
+++ tests/integration/test_plot.py
@@ -0,0 +1,55 @@
+"""
+needs pytest-regression package.
+run by `pythom -m test tests/`
+force regen image by `pythom -m test tests/ --force-regen`
+see also: https://pytest-regressions.readthedocs.io/en/latest/index.html
+"""
+
+from prettymaps import plot
+from prettymaps.new_api import GetArg, get_gdfs, plot_gdfs, read_preset, transform_gdfs
+
+# plot parameters
+DIFF_THRESHOLD = 3  # % of difference between images
+QUERY = "barra da tijuca"
+DILATE = (0,)
+PRESET = "tijuca"  # using this style since not inlucde palette key which introduce randomness of color
+RADIOUS = 300  # restrict radius for speed
+
+# regression image name
+REG_IMAGE = "plot_from_old_api"
+
+
+def test_regression_plot_from_old(image_regression, original_datadir):
+    """
+    tests old plot api: prettymaps.plot is generating same plot as before.
+    """
+
+    image = original_datadir / "tmp-tijuca-300.png"
+    print(f"{str(image)=}")
+    plot(QUERY, radius=RADIOUS, save_as=str(image), preset=PRESET)
+    image_regression.check(
+        image.read_bytes(), basename=REG_IMAGE, diff_threshold=DIFF_THRESHOLD
+    )
+
+
+def test_regression_plot_from_new_and_from_old(image_regression, original_datadir):
+    preset = read_preset(PRESET)
+
+    image = original_datadir / "tmp-tijuca-300-new-api.png"
+    print(str(image))
+
+    gdfs = get_gdfs(
+        GetArg(query=QUERY, radius=RADIOUS, dilate=preset.dilate, layers=preset.layers)
+    )
+
+    # do nothing
+    gdfs_transformed = transform_gdfs(gdfs)
+    assert gdfs_transformed == gdfs
+
+    plot_gdfs(
+        gdfs_transformed, layers=preset.layers, style=preset.style, save_as=str(image)
+    )
+
+    image_regression.check(
+        image.read_bytes(), basename=REG_IMAGE, diff_threshold=DIFF_THRESHOLD
+    )
